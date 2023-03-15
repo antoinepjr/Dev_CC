ccc286f59e57df6f44db52242b03afb7aa4e4080
antoinepjr <107955002+antoinepjr@users.noreply.github.com>
2023-03-15 00:41:48 -0400

    Delete docs directory

--- docs/Database_Inserts/AP_SoundCloud.sql
+++
@@ -1,5 +0,0 @@
---INSERT INTO playlist(plat_id, cur_id, embedLink, playlistGrouping, playlistName)
---VALUES ('3', '1', '', 'Fre$h', '');
-
-INSERT INTO playlist(plat_id, cur_id, embedLink, playlistGrouping, playlistName)
-VALUES ('3', '1', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/763781136%3Fsecret_token%3Ds-bXecy&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true', 'Fre$h', 'Fre$h');
